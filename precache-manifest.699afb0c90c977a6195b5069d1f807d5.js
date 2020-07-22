df0a8f0e69b574fddd88535c184a904ddb9b295c
Zishan Ansari <zshan.ansari07@gmail.com>
2020-07-22 18:25:15 +0530

    Updates

---
+++ precache-manifest.699afb0c90c977a6195b5069d1f807d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a340f3858cd78894e92911e938b004",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "dad991f905f46f20b724",
+    "url": "/my-app/static/css/main.07e12a12.chunk.css"
+  },
+  {
+    "revision": "0d82358e3adef1af4535",
+    "url": "/my-app/static/js/2.9212f4c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.9212f4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad991f905f46f20b724",
+    "url": "/my-app/static/js/main.8a1dad39.chunk.js"
+  },
+  {
+    "revision": "5b24d5225ee38413134f",
+    "url": "/my-app/static/js/runtime-main.c6bffc9e.js"
+  }
+]);
