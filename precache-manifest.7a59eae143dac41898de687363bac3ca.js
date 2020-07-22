67363f9841087b8ebfaf84d29189d8d08561f5b0
Zishan Ansari <zshan.ansari07@gmail.com>
2020-07-22 18:29:34 +0530

    Updates

---
+++ precache-manifest.7a59eae143dac41898de687363bac3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15e9cac0929154505e34068d1848efc",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "7fc1d5053824dd8db86c",
+    "url": "/todos/static/css/main.07e12a12.chunk.css"
+  },
+  {
+    "revision": "0d82358e3adef1af4535",
+    "url": "/todos/static/js/2.9212f4c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todos/static/js/2.9212f4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc1d5053824dd8db86c",
+    "url": "/todos/static/js/main.cb34a0f3.chunk.js"
+  },
+  {
+    "revision": "9a423b9131899688fce6",
+    "url": "/todos/static/js/runtime-main.d4b23622.js"
+  }
+]);
